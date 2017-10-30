67b1a7642a99cbf58f014be1761d32dbda05a723
Michael Jordan <michaeljordan@Michaels-MacBook-Pro.local>
2017-10-30 07:55:52 -0700

    fixed undefined id

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,33 +1,22 @@
-/*
- * action types
- */
-
-export const ADD_TODO = 'ADD_TODO'
-export const TOGGLE_TODO = 'TOGGLE_TODO'
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
-
-/*
- * other constants
- */
-
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
+let nextTodoId = 0
+export const addTodo = text => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  }
 }
 
-/*
- * action creators
- */
-
-export function addTodo(text) {
-  return { type: ADD_TODO, text }
+export const setVisibilityFilter = filter => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
 }
 
-export function toggleTodo(index) {
-  return { type: TOGGLE_TODO, index }
-}
-
-export function setVisibilityFilter(filter) {
-  return { type: SET_VISIBILITY_FILTER, filter }
+export const toggleTodo = id => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
 }
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,4 +1,5 @@
 const todos = (state = [], action) => {
+
   switch (action.type) {
     case 'ADD_TODO':
       return [
